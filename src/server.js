737269b18c47b0408e3c3f659f90d7f96cf4fcc8
219WD <macgunmanmc@gmail.com>
2025-01-14 23:35:04 -0300

    correcciones diskefinales

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const emailRouter = require("./routes/emailRouter");
+const emailRouter = require("./router/emailRouter");
 
 // Configuración de la base de datos
 mongoose.connect(process.env.MONGO_URI || "mongodb+srv://kaspercanepa:TJae7gqBnSlQF3II@219emails.w0gev.mongodb.net/219emails", {
@@ -15,20 +15,17 @@ mongoose.connect(process.env.MONGO_URI || "mongodb+srv://kaspercanepa:TJae7gqBnS
 
 const server = express();
 
-// Middlewares
-server.use(express.json());
-server.use(cors());
-
-// Usar el router de email en la ruta específica
-server.use("/descuento", emailRouter);
-
-// Configuración de CORS
+// Configurar CORS solo una vez con opciones
 const corsOptions = {
   origin: ['https://219labs-descuento.vercel.app'], // Cambia según el dominio frontend
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
 };
-server.use(cors(corsOptions));
-server.use(express.json());
+
+server.use(cors(corsOptions)); // Aplica CORS de forma específica
+server.use(express.json()); // Habilita JSON en las solicitudes entrantes
+
+// Usar el router de email en la ruta específica
+server.use("/descuento", emailRouter);
 
 module.exports = server; // Exportar el servidor para que pueda ser usado en index.js
